c0da6a5da6764e3e663fe1c1b9188e829c0254dd
d3qnew <d3qnew2@163.com>
2017-10-09 13:36:20 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render(
+        'index',
+        {
+            title: 'Express',
+            content: 'mian.html'
+        }
+    );
+});
+
+router.get('/main',function (req,res) {
+    res.render('main',{
+        title:'main',
+        content:'xxx'
+    });
 });
 
 module.exports = router;
